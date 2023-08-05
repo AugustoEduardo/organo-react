065c8fdefd0921a756819ad26144215d7e349912
Eduardo <trad.eaad@gmail.com>
2023-08-05 17:24:06 -0300

    Começando a criar os cards de Colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import "./Colaborador.css"
+
+const Colaborador = () => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/augustoeduardo.png" alt="Augusto Eduardo"/>
+            </div>
+            <div className="rodape">
+                <h4>Eduardo</h4>
+                <h5>Front End</h5>
+            </div>
+        </div>
+
+    )
+}
+
+export default Colaborador
