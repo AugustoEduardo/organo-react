c5d2937141ee2b39cba749198d346901a76f7c05
Eduardo <trad.eaad@gmail.com>
2023-08-05 20:21:43 -0300

    Criando os cards de forma dinâmica

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import "./Colaborador.css"
 
-const Colaborador = () => {
+const Colaborador = (props) => {
     return (
         <div className="colaborador">
             <div className="cabecalho">
-                <img src="https://github.com/augustoeduardo.png" alt="Augusto Eduardo"/>
+                <img src={props.imagem} alt={props.nome}/>
             </div>
             <div className="rodape">
-                <h4>Eduardo</h4>
-                <h5>Front End</h5>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
             </div>
         </div>
 
