9f863b199faa3f795b1a0ba5e3474ea416ba93bc
Eduardo <trad.eaad@gmail.com>
2023-08-21 21:48:24 -0300

    Criando id's

--- src/App.js
+++ src/App.js
@@ -3,42 +3,102 @@ import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 import Rodape from './componentes/Rodape';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times,setTimes] = useState([
 
     {
-    nome: 'Programação',
-    cor: '#D9F7E9'
+      id: uuidv4(),
+      nome: 'Programação',
+      cor: '#D9F7E9'
     },
     {
-    nome: 'Front-End',
-    cor: '#E8F8FF'
+      id: uuidv4(),
+      nome: 'Front-End',
+      cor: '#E8F8FF'
     },
     {
-    nome: 'Data Science',
-    cor: '#F0F8E2'
+      id: uuidv4(),
+      nome: 'Data Science',
+      cor: '#F0F8E2'
     },
     {
-    nome: 'Devops',
-    cor: '#FDE7E8'
+      id: uuidv4(),
+      nome: 'Devops',
+      cor: '#FDE7E8'
     },
     {
-    nome: 'UX e Design',
-    cor: '#FAE9F5'
+      id: uuidv4(),
+      nome: 'UX e Design',
+      cor: '#FAE9F5'
     },
     {
-    nome: 'Mobile',
-    cor: '#FFF5D9'
+      id: uuidv4(),
+      nome: 'Mobile',
+      cor: '#FFF5D9'
     },
     {
-    nome: 'Inovação e Gestão',
-    cor: '#FFEEDF'
+      id: uuidv4(),
+      nome: 'Inovação e Gestão',
+      cor: '#FFEEDF'
     },
-  ])
+  ]);
 
-  const [colaboradores, setColaboradores] = useState([])
+  const inicial = [
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'EDUARDO AUGUSTO',
+      cargo: 'Aluno',
+      imagem: 'https://github.com/augustoeduardo.png',
+      time: times[6].nome
+    },
+  ]
+
+  const [colaboradores, setColaboradores] = useState(inicial);
 
   function deletarColaborador() {
     console.log("Apagando colaborador")
@@ -51,7 +111,7 @@ function App() {
         time.cor = cor;
       }
       return time;
-    }))
+    }));
   }
 
   return (
